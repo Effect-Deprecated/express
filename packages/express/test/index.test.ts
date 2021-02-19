ed50b1834ddc944d12060f65d93c7ee1fa5f0f41
Michael Arnaldi <arnaldimichael@gmail.com>
2021-02-19 13:03:49 +0000

    add test

--- packages/express/test/index.test.ts
+++ packages/express/test/index.test.ts
@@ -1,5 +1,29 @@
+import * as T from "@effect-ts/core/Effect"
+import * as Exit from "@effect-ts/core/Effect/Exit"
+import { pipe } from "@effect-ts/system/Function"
+
+import { LiveExpress, withExpressApp } from "../src"
+
 describe("Dummy", () => {
-  it("pass", () => {
-    expect(1).toEqual(1)
+  it("pass", async () => {
+    const host = "127.0.0.1"
+    const port = 31157
+    const body = { message: "ok" }
+
+    const exit = await pipe(
+      withExpressApp((app) =>
+        T.effectTotal(() => {
+          app.get("/", (_, res) => {
+            res.send(body)
+          })
+        })
+      ),
+      T.andThen(T.fromPromise(() => fetch(`http://${host}:${port}/`))),
+      T.chain((r) => T.fromPromise(() => r.json())),
+      T.provideSomeLayer(LiveExpress(host, port)),
+      T.runPromiseExit
+    )
+
+    expect(exit).toEqual(Exit.succeed(body))
   })
 })
