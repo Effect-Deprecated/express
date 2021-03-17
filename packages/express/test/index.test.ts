995c17d8be286021dac8a59caf8c7a0216461ffc
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-17 22:42:51 +0000

    add runtime to test

--- packages/express/test/index.test.ts
+++ packages/express/test/index.test.ts
@@ -1,29 +1,54 @@
 import * as T from "@effect-ts/core/Effect"
 import * as Exit from "@effect-ts/core/Effect/Exit"
-import { pipe } from "@effect-ts/system/Function"
+import * as L from "@effect-ts/core/Effect/Layer"
+import { pipe } from "@effect-ts/core/Function"
+import type { Has } from "@effect-ts/core/Has"
+import { tag } from "@effect-ts/core/Has"
 
 import { LiveExpress, withExpressApp } from "../src"
 
 describe("Dummy", () => {
   it("pass", async () => {
+    interface AppConfig {
+      _tag: "@demo/AppConfig"
+      body: { message: string }
+    }
+
+    const AppConfig = tag<AppConfig>()
+
+    const { body: accessBodyM } = T.deriveAccessM(AppConfig)(["body"])
+
+    const LiveAppConfig = L.fromEffect(AppConfig)(
+      T.effectTotal(() => ({ _tag: "@demo/AppConfig", body: { message: "ok" } }))
+    )
+
     const host = "127.0.0.1"
     const port = 31157
-    const body = { message: "ok" }
 
     const exit = await pipe(
       withExpressApp((app) =>
-        T.effectTotal(() => {
-          app.get("/", (_, res) => {
-            res.send(body)
-          })
-        })
+        T.runtime<Has<AppConfig>>()["|>"](
+          T.chain((runtime) =>
+            T.effectTotal(() => {
+              app.get("/", (_, res) => {
+                runtime.run(
+                  accessBodyM((body) =>
+                    T.effectTotal(() => {
+                      res.send(body)
+                    })
+                  )
+                )
+              })
+            })
+          )
+        )
       ),
       T.andThen(T.fromPromise(() => fetch(`http://${host}:${port}/`))),
       T.chain((r) => T.fromPromise(() => r.json())),
-      T.provideSomeLayer(LiveExpress(host, port)),
+      T.provideSomeLayer(LiveExpress(host, port)["+++"](LiveAppConfig)),
       T.runPromiseExit
     )
 
-    expect(exit).toEqual(Exit.succeed(body))
+    expect(exit).toEqual(Exit.succeed({ message: "ok" }))
   })
 })
